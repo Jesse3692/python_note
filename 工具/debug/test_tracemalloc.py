700c941763a4ae86233d6b808551c80425f54ae9
zhangweibo <jessechang2358@gmail.com>
2021-12-07 16:59:02 +0800

    tracemalloc 统计内存占用

---
+++ 工具/debug/test_tracemalloc.py
@@ -0,0 +1,21 @@
+"""标准库tracemalloc，统计内存占用"""
+import tracemalloc
+
+# 开始跟踪内存分配
+tracemalloc.start()
+
+a = list(range(100000))
+b = list(range(10))
+
+# 快照，当前内存分配
+snapshot = tracemalloc.take_snapshot()
+
+# 快照对象的统计
+top_stats = snapshot.statistics('lineno')
+
+# 查看总内存使用
+stats = snapshot.statistics('filename')
+
+print("[ Top 10 ]")
+for stat in stats[:10]:
+    print(stat)
